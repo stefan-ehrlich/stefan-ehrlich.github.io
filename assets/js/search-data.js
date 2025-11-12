adfe3018ef79e8698471a9d729bb88890fbe9e1e
stefan-ehrlich <43376399+stefan-ehrlich@users.noreply.github.com>
2025-11-12 16:56:22 +0000

    Deploying to gh-pages from @ stefan-ehrlich/stefan-ehrlich.github.io@8b44541733f862dcf3c4c24753f90c713d511605 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "post-a-post-with-plotly-js",
+  },{id: "nav-publications",
+          title: "publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
